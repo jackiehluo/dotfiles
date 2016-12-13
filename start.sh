eac99bf8137bb86d9765c08fbcf0f2d5fafd371d
Jackie Luo <jackiehluo@gmail.com>
2016-12-13 15:30:16 -0800

    Update git config and install brew

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
 git config --global alias.lg "log --color --graph --branches --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
